e28e1a5810d6933cfb563d1fdc151b2cff99b682
Stefan Rusterholz <stefan.rusterholz@gmail.com>
2011-03-26 23:59:39 +0100

    Updated test.rb to be indifferent about cwd.

--- example/test.rb
+++ example/test.rb
@@ -2,11 +2,12 @@ lib1 = File.expand_path("#{__FILE__}/../../lib")
 lib2 = File.expand_path("#{__FILE__}/../lib")
 $LOAD_PATH.unshift(lib1) unless $LOAD_PATH.include?(lib1)
 $LOAD_PATH.unshift(lib2) unless $LOAD_PATH.include?(lib2)
+rackup_file = File.expand_path('../rackup.ru', __FILE__)
 
 require 'rack'
 require 'browsr'
 
-app = Rack::Builder.parse_file('rackup.ru').first
+app = Rack::Builder.parse_file(rackup_file).first
 B   = Browsr.rack(app)
 
 puts "The browser is available via the constant 'B'"
